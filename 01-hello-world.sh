d7689874ee95e4f19404afb1f5b9066c254fdcdc
Anantha009 <srirudram09@gmail.com>
2025-08-12 11:30:44 +0530

    I made some changes

---
+++ 01-hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Ctrl+s
+echo "Hello, World!"
+echo "This is my first script in Git practice."
+echo "Let's learn Git together!"
+echo "Remember to commit your changes!"
+echo "Happy coding!"
+echo "End of script."
