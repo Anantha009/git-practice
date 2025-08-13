c52e4c95e3af7a995786b50a89326cc1372c1ff6
Anantha009 <srirudram09@gmail.com>
2025-08-13 14:56:04 +0530

    Add 08-datatypes.sh

---
+++ 08-datatypes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt user for two numbers
+echo "Enter first number:"
+read NUM1
+echo "Enter second number:"
+read NUM2
+
+# Add the numbers
+SUM=$((NUM1 + NUM2))
+
+# Display the result
+echo "The sum of $NUM1 and $NUM2
