b61383bfd6013b328320e57b40c2c44d1703d69a
Anantha009 <srirudram09@gmail.com>
2025-08-13 15:31:20 +0530

    Add 09-special-vars.sh

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "All variables passed to the script:"
-for var in "$@"; do
-  echo "$var"
-done
+echo "All variables passed to the script: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $(pwd)"
+echo "Home directory: $HOME"
